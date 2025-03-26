0773a4f9b6495d88b3c3e0df2dbec638c8e129f6
Fairgraves <rozaco0@gmail.com>
2025-03-26 16:47:02 +0700

    starting commit

---
+++ src/test/java/com/theorea/tms/TmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theorea.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
